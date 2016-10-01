0cd82be0b655170781bd3067c394571fdd7c8277
shikakun <k@u-n.jp>
2016-10-01 16:04:40 +0900

    環境変数を指定するファイルを用意して読み込むようにした

--- app.rb
+++ app.rb
@@ -5,6 +5,14 @@ require 'hamlit'
 
 set :haml, format: :html5
 
+envfile = File.expand_path(File.join(Dir.pwd, ".env"))
+if File.exists?(envfile)
+  File.open(envfile, "r").each do |line|
+    key, val = line.split("=", 2)
+    ENV[key] = val.chomp unless val.nil?
+  end
+end
+
 get '/' do
   haml :index
 end
